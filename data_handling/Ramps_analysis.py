b0afb48d399697cde062e568217e30b2b4d61f75
r.hawke <rebecca.hawke@measurement.govt.nz>
2020-12-07 17:00:01 +1300

    Ramps analysis as used for loflo mmts

--- data_handling/Ramps_analysis.py
+++ data_handling/Ramps_analysis.py
@@ -2,6 +2,7 @@
 Analysis of single steps to evaluate reproducibility of syringe pump.
 """
 import os
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -56,47 +57,53 @@ def separate_waits_from_ramp(sp_data, rfc_data):
 
 
 
-fol_steps = r'G:\Shared drives\MSL - Shared\MSL Kibble Balance\_p_PressureManifoldConsiderations\Flow and Pressure control\SyringePumpTests\20201123_Steps_0p2mL'
+fol_steps = r'G:\Shared drives\MSL - Shared\MSL Kibble Balance\_p_PressureManifoldConsiderations\Flow and Pressure control\SyringePumpTests\20201201_Step_loflo'
 
-# files = get_all_fnames(fol_steps, "Step_", endpattern="_LVDT.xlsx")
+files = get_all_fnames(fol_steps, "Step_", endpattern="_LVDT.xlsx")
 
 summary = "Summary_step_fits.xlsx"
 f1 = os.path.join(fol_steps, summary)
 xls = pd.ExcelFile(f1)
-starts = pd.read_excel(xls, 'Raw_LVDT_lo')
+starts = pd.read_excel(xls, 'Raw_LVDT_leak')
 
-for i, fname in enumerate(starts['Filename']):
+for i, fname in enumerate(starts['fname']):
+# for i, fname in enumerate(files):
     name_parts = fname.split("_")
     vol = float(name_parts[1])
     flo = float(name_parts[2])
     ts = name_parts[3]
-    time_int = 0.02
 
     sp_data, rfc_data = read_in_data(fol_steps, fname)
 
     x = rfc_data["Timestamp"][1:]
-    y = rfc_data["LVDT (mm)"][1:]
-    x_i = np.linspace(0, 0.02 * len(x), len(x))
+    time_delta = datetime.strptime(x[2], '%Y-%m-%d %H:%M:%S.%f')-datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S.%f')
+    time_int = time_delta.total_seconds()
+    x_i = np.linspace(0, time_int * len(x), len(x))
+
+    y = rfc_data["Height (mm)"][1:]
     # plt.plot(y)
     # plt.show()
 
-    start = int(starts['start'][i]) #int(input("start"))
-    num_pts = int(abs(vol)/flo*60/time_int)
+    wait = int(0 / time_int) # int(input("start"))
+    start = int(starts['start'][i]) + wait
+    end = int(starts['end'][i])
+    num_pts = end - start
+    wait_pts = int(20 / time_int)
 
     # Initial fall rate
-    pars_pre = fit_linear(x_i[0:start], y[0:start])
+    pars_pre = fit_linear(x_i[0:wait_pts], y[0:wait_pts])
 
     # Ramp rate
     pars = fit_linear(x_i[start:start+num_pts], y[start:start+num_pts])
 
     # Final fall rate
-    end_pts = int(20/0.02)
-    pars_post = fit_linear(x_i[start+num_pts+50:], y[start+num_pts+50:])
+
+    pars_post = fit_linear(x_i[start+num_pts:], y[start+num_pts:])
 
     plt.show()
 
     deltaH = [(pars_post[0] - pars_pre[0]) * x + pars_post[1] - pars_pre[1] for x in x_i]
-    dh_ave = np.average(deltaH[start:start+num_pts])
+    dh_ave = deltaH[start+int(num_pts/2)]
     dh_std_dev = np.std(deltaH[start:start+num_pts], ddof=1)
 
     print(fname, start, num_pts, vol/abs(vol), flo, *pars, *pars_pre, *pars_post, dh_ave, dh_std_dev)
